9e4202417f803f5de0fb36bcf3faae9478fd5347
Mikoprn <b4gas24@gmail.com>
2025-05-16 12:41:24 +0700

    add new style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: passthroughImageService(),
     domains: ["astro.build"],
   },
+
+  integrations: [sitemap()],
 });
