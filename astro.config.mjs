6aa79f24ccbb7621d0f7b887d8c8bfc04fe0f1e2
Mikoprn <b4gas24@gmail.com>
2025-05-16 12:43:00 +0700

    add new

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://chrisxyz.vercel.app/',
   image: {
     service: passthroughImageService(),
     domains: ["astro.build"],
